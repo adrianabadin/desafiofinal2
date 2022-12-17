995f66ad8284f99dbe6f545160a189ba0b38bd4f
Adrian Daniel Abadin <aabadin@gmail.com>
2022-12-17 06:11:52 -0300

    Codigo transpilado de TS a JS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const products = require('./routes/products')
 const cart = require('./routes/cart')
 const PORT = process.env.PORT || 8080
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 app.use(express.urlencoded({ extended: false }))
 
 process.env.storage = 'sqlite'
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,9 +1,8 @@
 const Router = require('express').Router
-const CartControllers = require('../../controllers/cart.controller')
+const CartControllers = require('../controllers/cart.controller')
 const cartController = CartControllers()
 const cartRoutes = Router()
 cartRoutes.delete('/:id', cartController.deleteCart)
-
 cartRoutes.get('/:id', cartController.getCart)
 cartRoutes.post('/', cartController.createCart)
 cartRoutes.post('/:id/products', cartController.addProduct)
